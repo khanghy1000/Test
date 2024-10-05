80fa1dbad8595c0dae27e5dd8c0e55a2b17d3d3c
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2024-10-05 22:50:11 +0700

    Auto commit

--- .config/nvim/lua/custom/plugins/colorscheme.lua
+++ .config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,22 +1,36 @@
--- return {
---   'rebelot/kanagawa.nvim',
---   priority = 1000, -- Make sure to load this before all the other start plugins.
---   opts = {
---     transparent = true,
---   },
---   init = function()
---     vim.cmd.colorscheme 'kanagawa-dragon'
---
---     -- You can configure highlights by doing something like:
---     vim.cmd.hi 'Comment gui=none'
---   end,
--- }
-
 return {
-  'EdenEast/nightfox.nvim',
-  priority = 1000,
-  opts = { options = { transparent = true } },
-  init = function()
-    vim.cmd.colorscheme 'nightfox'
-  end,
+  {
+    'rebelot/kanagawa.nvim',
+    enabled = false,
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    opts = {
+      transparent = true,
+    },
+    init = function()
+      vim.cmd.colorscheme 'kanagawa-dragon'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+  {
+    'EdenEast/nightfox.nvim',
+    enabled = false,
+    priority = 1000,
+    opts = { options = { transparent = true } },
+    init = function()
+      vim.cmd.colorscheme 'nightfox'
+    end,
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+    },
+    init = function()
+      vim.cmd.colorscheme 'catppuccin-macchiato'
+    end,
+  },
 }
