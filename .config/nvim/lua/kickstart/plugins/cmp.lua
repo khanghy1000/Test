2de3b7fe5cf0f1e2eb61f4f1024d115b188e10b3
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2024-10-05 13:27:18 +0700

    Auto commit

--- .config/nvim/lua/kickstart/plugins/cmp.lua
+++ .config/nvim/lua/kickstart/plugins/cmp.lua
@@ -120,13 +120,13 @@ return {
         },
 
         sources = {
-          { name = 'copilot' },
           {
             name = 'lazydev',
             -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
             group_index = 0,
           },
           { name = 'nvim_lsp' },
+          { name = 'copilot' },
           { name = 'luasnip' },
           { name = 'path' },
           -- buffer words from all buffers
