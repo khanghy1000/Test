daf5bb982b1eee1886d1c90d51d49a41f17aeb3d
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2024-11-02 21:19:30 +0700

    Auto commit

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -18,17 +18,22 @@ config.enable_tab_bar = false
 config.window_background_opacity = 0.95
 config.window_close_confirmation = 'NeverPrompt'
 
+config.color_scheme = 'Aura (Gogh)'
 config.colors = {
-  foreground = '#CBE0F0',
   background = '#151515',
-  cursor_bg = '#47FF9C',
-  cursor_border = '#47FF9C',
-  cursor_fg = '#011423',
-  selection_bg = '#033259',
-  selection_fg = '#CBE0F0',
-  ansi = { '#214969', '#E52E2E', '#44FFB1', '#FFE073', '#0FC5ED', '#a277ff', '#24EAF7', '#24EAF7' },
-  brights = { '#214969', '#E52E2E', '#44FFB1', '#FFE073', '#A277FF', '#a277ff', '#24EAF7', '#24EAF7' },
 }
+
+-- config.colors = {
+--   foreground = '#CBE0F0',
+--   background = '#151515',
+--   cursor_bg = '#47FF9C',
+--   cursor_border = '#47FF9C',
+--   cursor_fg = '#011423',
+--   selection_bg = '#033259',
+--   selection_fg = '#CBE0F0',
+--   ansi = { '#214969', '#E52E2E', '#44FFB1', '#FFE073', '#0FC5ED', '#a277ff', '#24EAF7', '#24EAF7' },
+--   brights = { '#214969', '#E52E2E', '#44FFB1', '#FFE073', '#A277FF', '#a277ff', '#24EAF7', '#24EAF7' },
+-- }
 
 config.window_padding = {
   left = '1cell',
